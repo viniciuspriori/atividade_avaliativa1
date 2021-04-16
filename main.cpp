eb12a74fbada281a3123f01cf889f246851a5764
Vinicius Priori <>
2021-04-16 04:38:40 +0000

    limite de palavras

--- main.cpp
+++ main.cpp
@@ -29,38 +29,50 @@ int main() {
   cout << endl;
   cout << "digite uma palavra" << endl; //entrada 
   cin >> palavra;
-
- for(int i=0; i<size(palavra); i++)
-    {
-      //casos especiais: s e z (fora da matriz)
-      if(palavra[i]=='s')
+  
+   do{
+      for(int i=0; i<size(palavra); i++)
         {
-        np=7;
-        nv=4; 
+          if(size(palavra)>50)
+          {
+            i--;
+            cout << "palavra muito longa!" << endl;
+            cout << "digite uma palavra" << endl; //entrada 
+            cin >> palavra;
 
-        cout << "#" << np << "=" << nv << endl;
-        }
-        if(palavra[i]=='z')
-        {
-        np=9;
-        nv=4;
+          } else{
+      
+          //casos especiais: s e z (fora da matriz)
+          if(palavra[i]=='s')
+            {
+            np=7;
+            nv=4; 
 
-        cout << "#" << np << "=" << nv << endl;
-        }
-
-        //percorrer a matriz para achar a letra
-        for(int c=0; c<=7; c++){
-          for(int l=0; l<=2; l++){
-
-            if(palavra[i] == alfa[l][c])
-              {
-              //transformar matriz - ex. LETRA A: posição 0,0 = 2,1 (apertar nº2 1x)
-              np = c+2; //somar 2 na coluna 
-              nv = l+1; // somar 1 na linha
-              cout << "#" << np << "=" << nv << endl;
-              } 
+            cout << "#" << np << "=" << nv << endl;
             }
-          }
-     }
- return 0;
+            if(palavra[i]=='z')
+            {
+            np=9;
+            nv=4;
+
+            cout << "#" << np << "=" << nv << endl;
+            }
+
+            //percorrer a matriz para achar a letra
+            for(int c=0; c<=7; c++){
+              for(int l=0; l<=2; l++){
+
+                if(palavra[i] == alfa[l][c])
+                  {
+                  //transformar matriz - ex. LETRA A: posição 0,0 = 2,1 (apertar nº2 1x)
+                  np = c+2; //somar 2 na coluna 
+                  nv = l+1; // somar 1 na linha
+                  cout << "#" << np << "=" << nv << endl;
+                  } 
+                }
+              }
+         }
+      }
+    } while(size(palavra)>50);
+  return 0;
 }
